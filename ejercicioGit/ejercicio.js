d3e98f5185d1c6cfbc5d9ee0d955e0a62800c0d7
paucarima <paucarimap@gmail.com>
2024-01-26 22:26:30 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./img/ganarpapa.gif"
     console.log("Ingresa a saludar");
 }
