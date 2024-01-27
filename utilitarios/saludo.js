4b981bbfdc5fd2b2fadd549e1681c2cb57ab230c
paucarima <paucarimap@gmail.com>
2024-01-27 18:33:24 -0500

    funcion imagen completa

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -7,6 +7,22 @@ saludar = function(){
     let edad=recuperarInt("txtEdad");
 
     let estatura=recuperarFloat("txtEstatura");
+
+    let mensajeBienvenida="Bienvenido "+nombre+" "+apellido;
+
+    mostrarTexto("lblResultado",mensajeBienvenida);
+    mostrarImagen("imgSaludo","./imagenes/ralf.gif");
+}
+
+mostrarImagen=function(idComponente,rutaImagen){
+  let componente;
+  componente=document.getElementById(idComponente);
+  componente.src= rutaImagen;
+}
+mostrarTexto=function(idComponente,mensaje){
+  let componente;
+  componente=document.getElementById(idComponente);
+  componente.innerText= mensaje;
 }
 
 recuperarTexto=function(idComponente){
