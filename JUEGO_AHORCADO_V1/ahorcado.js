285dec01faa790ab35d86cf31da127e619f28e88
paucarima <paucarimap@gmail.com>
2024-02-14 19:48:55 -0500

    PASO 2 Mostrar la letra

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -3,24 +3,50 @@ let palabraSecreta;
 
 esMayuscula = function (caracter) {
     let codigoLetra;
-    for(let i=0; i<caracter.length; i++){
-        codigoLetra=caracter.charCodeAt(i);
+    for (let i = 0; i < caracter.length; i++) {
+        codigoLetra = caracter.charCodeAt(i);
         if (codigoLetra >= 65 && codigoLetra <= 90) {
             return true;
         } else {
             return false;
         }
     }
-    console.log("Mayusculas: "+codigoLetra);  
+    console.log("Mayusculas: " + codigoLetra);
 }
 
-guardarPalabra=function () {
-   let palabra= recuperarTexto("txtSecreta");
-   if (!(palabra.length==5) && !(esMayuscula(palabra)==false)) {
-      alert("La contrasenia debe tener 5 letras mayusculas");
-      
-   } else {
-    palabraSecreta=palabra;
-    console.log("Guardada: " +palabraSecreta);
-   }
+guardarPalabra = function () {
+    let palabra = recuperarTexto("txtSecreta");
+    if (!(palabra.length == 5) && !(esMayuscula(palabra) == false)) {
+        alert("La contrasenia debe tener 5 letras mayusculas");
+
+    } else {
+        palabraSecreta = palabra;
+        console.log("Guardada: " + palabraSecreta);
+    }
+}
+
+mostrarLetra = function (letra, posicion) {
+
+    if (posicion == 0) {
+
+        mostrarTexto("div0", letra);
+    }
+    if (posicion == 1) {
+
+        mostrarTexto("div1", letra);
+    }
+    if (posicion == 2) {
+
+        mostrarTexto("div2", letra);
+    }
+    if (posicion == 3) {
+
+        mostrarTexto("div3", letra);
+    }
+    if (posicion == 4) {
+
+        mostrarTexto("div4", letra);
+    }
+
+
 }
