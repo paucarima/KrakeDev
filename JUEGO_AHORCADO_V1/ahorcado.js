6585d062c464695322ab1a73c6e4df0e8bd1b073
paucarima <paucarimap@gmail.com>
2024-02-15 01:40:20 -0500

    PASO 5 6 7 ALERT, mostrarAhorcado, gif :D

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,7 +1,8 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 let palabraSecreta;
-let letrasEncontradas=0;
-
+let letrasEncontradas = 0;
+let intentos = 0;
+let errores = 0;
 esMayuscula = function (caracter) {
     let codigoLetra;
     for (let i = 0; i < caracter.length; i++) {
@@ -23,6 +24,7 @@ guardarPalabra = function () {
     } else {
         palabraSecreta = palabra;
         console.log("Guardada: " + palabraSecreta);
+        alert("Palabra Guardada");
     }
 }
 
@@ -52,25 +54,81 @@ mostrarLetra = function (letra, posicion) {
 }
 
 validar = function (letra) {
-     
+    let encontrado=false;
+
     for (let posicion = 0; posicion < palabraSecreta.length; posicion++) {
-        
+
         let caracterSecreto = palabraSecreta.charAt(posicion);
-        let caracterLetra=letra;
-        if (caracterSecreto==caracterLetra) {
-            mostrarLetra(caracterLetra,posicion);
+        let caracterLetra = letra;
+        if (caracterSecreto == caracterLetra) {
+            mostrarLetra(caracterLetra, posicion);
             letrasEncontradas++;
+            encontrado=true;
+
             //console.log("El CARACTER " + caracterLetra+ " posicion " + posicion);
         }
 
     }
+  
+
+
+    if (!encontrado) {
+        alert("LA LETRA NO ES PARTE DE LA PALABRA");
+        errores++;
+        mostrarAhorcado();
+    }
+
+
+
+
 }
 
-ingresarLetra=function(){
-     let letra=recuperarTexto("txtLetra");
-     if(esMayuscula(letra)==true && letra.length==1){
+ingresarLetra = function () {
+    let letra = recuperarTexto("txtLetra");
+    if (esMayuscula(letra) == true && letra.length == 1) {
         validar(letra);
-     }else{
+        intentos++;
+    } else {
         alert("SOLO SE ACEPTA UNA LETRA MAYUSCULA");
-     }
+    }
+
+    if (letrasEncontradas == 5) {
+        mostrarImagen("ahorcadoImagen", "ganador.gif");
+    }
+    if (intentos == 10) {
+        mostrarImagen("ahorcadoImagen", "gameOver.gif");
+    }
+
+
+}
+
+mostrarAhorcado = function () {
+    if (errores == 1) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_01.png");
+    }
+    if (errores == 2) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_02.png");
+    }
+    if (errores == 3) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_03.png");
+    }
+    if (errores == 4) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_04.png");
+    }
+    if (errores == 5) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_05.png");
+    }
+    if (errores == 6) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_06.png");
+    }
+    if (errores == 7) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_07.png");
+    }
+    if (errores == 8) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_08.png");
+    }
+    if (errores == 9) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_09.png");
+    }
+
 }
